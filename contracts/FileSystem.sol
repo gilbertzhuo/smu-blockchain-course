b45986e7ce04942394b1db863f193f4bdbbb9170
gilbertzhuo <gilbertzhuo@gmail.com>
2022-05-14 17:07:16 +0800

    made changes to the smart contract

--- contracts/FileSystem.sol
+++ contracts/FileSystem.sol
@@ -1,25 +1,43 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >= 0.5.16;
+pragma solidity >=0.5.16;
 pragma experimental ABIEncoderV2;
 
 contract FileSystem {
-
-    struct item{
+    struct item {
         string name;
-        string course;
-        string issuer; 
+        string sex;
+        string ic;
+        string dob;
+        string vaccination_status;
         string date;
     }
 
     mapping(string => item) public store;
 
-    event data(string id, string name, string course, string issuer, string date);
-    function createCert(string memory id, string memory name, string memory course, string memory issuer, string memory date) public {
-        store[id] = item(name, course, issuer, date);
-        emit data(id, name, course, issuer, date);
+    event data(
+        string id,
+        string sex,
+        string name,
+        string ic,
+        string dob,
+        string date,
+        string vaccination_status
+    );
+
+    function createCert(
+        string memory id,
+        string memory sex,
+        string memory name,
+        string memory ic,
+        string memory dob,
+        string memory date,
+        string memory vaccination_status
+    ) public {
+        store[id] = item(name, sex, ic, dob, vaccination_status, date);
+        emit data(id, name, sex, ic, dob, vaccination_status, date);
     }
 
-    function fetchCert(string memory id) public view returns(item memory) {
+    function fetchCert(string memory id) public view returns (item memory) {
         return store[id];
     }
 }
